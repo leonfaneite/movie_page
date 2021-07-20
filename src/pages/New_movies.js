336896cb695f605682d83bac2677ab029719cf97
usuario <usuario@localhost.localdomain>
2021-07-20 00:15:40 -0500

    creacio de peliculas nuevas  incompleto falta paginacion

--- src/pages/New_movies.js
+++ src/pages/New_movies.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "antd";
+import {API_URL, TOKEN_API} from "../utils/Constants"
+import Footer from "../components/Footer";
+import Loading from "../components/Loading";
+import MovieCatalog from "../components/MovieCatalog";
 
-export default function New_movies(){
 
-return(
+export default function NewMovies() {
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
 
-<h1>estoy en   New_movies</h1>
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `${API_URL}/movie/now_playing?api_key=${TOKEN_API}&lenguage=es-ES&page=${page}`
+      );
+      const movies = await response.json();
+      setMovieList(movies);
+    })();
+  }, [page]);
 
+  const onChangePage = page => {
+    setPage(page);
+  };
 
-)}
+  return (
+    <Row>
+      <Col span="24" style={{ textAlign: "center", marginTop: 25 }}>
+        <h1 style={{ fontSize: 35, fontWeight: "bold" }}>
+          Ultimos lanzamientos
+        </h1>
+      </Col>
+      {movieList.results ? (
+        <Row>
+          <Col span="24">
+            <MovieCatalog movies={movieList} />
+          </Col>
+          
+        </Row>
+      ) : (
+        <Col span="24">
+          <Loading />
+        </Col>
+      )}
+      <Col span={24}>
+        <Footer />
+      </Col>
+    </Row>
+  );
+}
